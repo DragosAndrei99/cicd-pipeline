3506ad2d783e6fb66e3b4613b295135c5b0ac199
DragosAndrei99 <vdragos1999@yahoo.com>
2023-12-04 11:38:47 +0200

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
-from my_pipeline.my_pipeline_stack import MyPipelineStack
+from cicd_pipeline.cicd_pipeline_stack import MyPipelineStack
 
 app = cdk.App()
 MyPipelineStack(app, "MyPipelineStack",
