32e06956d655e883bb7f348d50c335d317c9af2a
DragosAndrei99 <vdragos1999@yahoo.com>
2023-12-04 11:46:19 +0200

    fix account id, region and repo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from cicd_pipeline.cicd_pipeline_stack import MyPipelineStack
 
 app = cdk.App()
 MyPipelineStack(app, "MyPipelineStack",
-    env=cdk.Environment(account="111111111111", region="eu-west-1")
+    env=cdk.Environment(account="576973527573", region="us-east-1")
 )
 
 app.synth()
