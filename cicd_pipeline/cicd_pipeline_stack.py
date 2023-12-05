5de025a22c118f5b7c37be90ed2c4011aea11175
DragosAndrei99 <vdragos1999@yahoo.com>
2023-12-05 10:21:34 +0200

    Add lambda stack and unit tests

--- cicd_pipeline/cicd_pipeline_stack.py
+++ cicd_pipeline/cicd_pipeline_stack.py
@@ -1,25 +1,27 @@
 import aws_cdk as cdk
 from constructs import Construct
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
-from cicd_pipeline.cicd_pipeline_app_stage import MyPipelineAppStage
+from cicd_pipeline.cicd_pipeline_app_stage import LambdaAppStage
 
 class MyPipelineStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        source = CodePipelineSource.git_hub(
+                                "DragosAndrei99/cicd-pipeline",
+                                "main",
+                                authentication=cdk.SecretValue.secrets_manager("github-key"))
+
         pipeline =  CodePipeline(self, "Pipeline",
                         pipeline_name="MyPipeline",
                         synth=ShellStep("Synth",
-                            input=CodePipelineSource.git_hub(
-                                "DragosAndrei99/cicd-pipeline",
-                                "main",
-                                authentication=cdk.SecretValue.secrets_manager("github-key")),
+                            input=source,
                             commands=["npm install -g aws-cdk",
                                 "python -m pip install -r requirements.txt",
                                 "cdk synth"]
                         )
                     )
-        
-        pipeline.add_stage(MyPipelineAppStage(self, "CustomStage",
+        # adding the stage to our pipeline
+        lambda_function = pipeline.add_stage(LambdaAppStage(self, "CustomStage",
             env=cdk.Environment(account="576973527573", region="us-east-1")))
--- tests/unit/test_cicd_pipeline_stack.py
+++ tests/unit/test_cicd_pipeline_stack.py
@@ -1,15 +1,19 @@
-import aws_cdk as core
-import aws_cdk.assertions as assertions
+from aws_cdk import core
+from cicd_pipeline.cicd_pipeline_lambda_stack import MyLambdaStack
 
-from cicd_pipeline.cicd_pipeline_stack import CicdPipelineStack
+def test_lambda_handler():
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in cicd_pipeline/cicd_pipeline_stack.py
-def test_sqs_queue_created():
+    # GIVEN
     app = core.App()
-    stack = CicdPipelineStack(app, "cicd-pipeline")
-    template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    # WHEN
+    MyLambdaStack(app, 'Stack', 'UnitTestTag')
+
+    # THEN
+    template = app.synth().get_stack_by_name('Stack').template
+    functions = [resource for resource in template['Resources'].values()
+                if resource['Type'] == 'AWS::Lambda::Function']
+
+    assert len(functions) == 1
+    assert functions[0]['Properties']['MemorySize'] == 1024
+    assert functions[0]['Properties']['Timeout'] == 30
