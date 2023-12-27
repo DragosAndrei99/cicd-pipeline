ebd4ea41fb2f8d5efaab8db830fd46e5cdf7ea2d
DragosAndrei99 <vdragos1999@yahoo.com>
2023-12-27 10:50:08 +0200

    Assume custom role

--- stacks/cicd_pipeline_stack.py
+++ stacks/cicd_pipeline_stack.py
@@ -2,6 +2,7 @@ import aws_cdk as cdk
 from constructs import Construct
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 from stacks.cicd_pipeline_app_stage import ApiGWHttpApiLambdaDynamodbStage
+import aws_cdk.aws_iam as iam
 
 
 class MyPipelineStack(cdk.Stack):
@@ -9,6 +10,14 @@ class MyPipelineStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        role = iam.Role(self, "Role",
+          assumed_by=iam.ServicePrincipal("codebuild.amazonaws.com")) # required
+
+        role.add_to_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            resources=["*"],
+            actions=["ec2:DescribeAvailabilityZones"]
+        ))
         source = CodePipelineSource.git_hub(
             "DragosAndrei99/cicd-pipeline",
             "main",
@@ -21,7 +30,8 @@ class MyPipelineStack(cdk.Stack):
                                                 commands=["npm install -g aws-cdk",
                                                           "python -m pip install -r requirements.txt",
                                                           "cdk synth"]
-                                                )
+                                                ),
+                                role=role
                                 )
         # adding the stage to our pipeline
         lambda_function = pipeline.add_stage(ApiGWHttpApiLambdaDynamodbStage(self, "ApiGWHttpApiLambdaDynamodbStage",
