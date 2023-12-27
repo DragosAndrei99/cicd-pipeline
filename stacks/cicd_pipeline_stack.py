ba117029fe0fdb596bb07762c352fa53792db204
DragosAndrei99 <vdragos1999@yahoo.com>
2023-12-27 11:04:28 +0200

    Remove custom role

--- stacks/cicd_pipeline_stack.py
+++ stacks/cicd_pipeline_stack.py
@@ -10,14 +10,6 @@ class MyPipelineStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        role = iam.Role(self, "Role",
-          assumed_by=iam.ServicePrincipal("codebuild.amazonaws.com")) # required
-
-        role.add_to_policy(iam.PolicyStatement(
-            effect=iam.Effect.ALLOW,
-            resources=["*"],
-            actions=["ec2:DescribeAvailabilityZones"]
-        ))
         source = CodePipelineSource.git_hub(
             "DragosAndrei99/cicd-pipeline",
             "main",
@@ -30,9 +22,8 @@ class MyPipelineStack(cdk.Stack):
                                                 commands=["npm install -g aws-cdk",
                                                           "python -m pip install -r requirements.txt",
                                                           "cdk synth"]
-                                                ),
-                                role=role
-                                )
+                                                )
+                                        )
         # adding the stage to our pipeline
         lambda_function = pipeline.add_stage(ApiGWHttpApiLambdaDynamodbStage(self, "ApiGWHttpApiLambdaDynamodbStage",
                                                             env=cdk.Environment(account="576973527573", region="us-east-1")))
